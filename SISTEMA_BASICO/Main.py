bfde0345d6c47f6f3678692dc217502331d3a267
Sergio Perea <spc00033@red.ujaen.es>
2021-02-15 18:22:54 +0100

    Añadido tiempo de ejecución de pr1.1

--- SISTEMA_BASICO/Main.py
+++ SISTEMA_BASICO/Main.py
@@ -2,14 +2,17 @@
 from filtrado import Filtrado
 import os
 from os.path import isfile,join
+import time
 
-ruta = "C:\CODIGO\SRI\PRACTICAS_SRI\SISTEMA_BASICO\Colección_SRI_2021"
-contenido = os.listdir(ruta)
+rutaColeccion = "C:\CODIGO\SRI\PRACTICAS_SRI\SISTEMA_BASICO\Colección_SRI_2021"
+contenido = os.listdir(rutaColeccion)
+rutaLimpios = "C:\CODIGO\SRI\PRACTICAS_SRI\SISTEMA_BASICO\Limpieza_SRI_2021"
 
 #Obtener los archivos de la carpeta
-archivos = [nombre for nombre in contenido if isfile(join(ruta,nombre))]
-#print(archivos)
+archivos = [nombre for nombre in contenido if isfile(join(rutaColeccion,nombre))]
 
+start_time = time.time()
 for archivo in archivos:
-    filtro = Filtrado(join(ruta,archivo))
-    filtro.normalizacion_tokenizacion()
+    filtro = Filtrado(join(rutaColeccion,archivo))
+    filtro.normalizacion_tokenizacion(rutaLimpios)
+print("El programa ha tardado ",(time.time() - start_time), " segundos en ejecutarse")
--- SISTEMA_BASICO/filtrado.py
+++ SISTEMA_BASICO/filtrado.py
@@ -9,20 +9,17 @@ class Filtrado(object):
         self.identificador = self.doc.getElementsByTagName("identifier")[0] #identificador del archivo
         self.fecha = self.doc.getElementsByTagName("dc:date")[0] #Fecha del documento
         self.nombre = self.doc.getElementsByTagName("dc:title")[0] #Nombre en español del documento
-        
         #self.autores = doc.getElementsByTagName("dc:creator") #Contenedor de autores
         #self.temas = doc.getElementsByTagName("dc:subject") #Temas relacionados con el documento
         self.fuente = self.doc.getElementsByTagName("dc:source")[0] #Fuente de donde proviene la información. Interesante a la hora de posicionar según la fuente.
-        print(self.nombre.firstChild.data)
-        print(self.fecha.firstChild.data, " :: ", self.fuente.firstChild.data)
         self.cuerpo = self.doc.getElementsByTagName("dc:description")[0]
         self.informacion = [self.nombre, self.fecha, self.fuente, self.cuerpo]
 
 
-    def normalizacion_tokenizacion(self):
+    def normalizacion_tokenizacion(self,ruta):
         aux = self.identificador.firstChild.data
         nombre_archivo = aux.split(":")
-        archivo = open(join("C:\CODIGO\SRI\PRACTICAS_SRI\SISTEMA_BASICO\Limpieza_SRI_2021",nombre_archivo[len(nombre_archivo)-1]) +".txt","w")
+        archivo = open(join(ruta,nombre_archivo[len(nombre_archivo)-1]) +".txt","w")
         no_borrar = ['0','1','2','3','4','5','6','7','8','9','-','_','']
         for i in self.informacion:
             aux = i.firstChild.data
